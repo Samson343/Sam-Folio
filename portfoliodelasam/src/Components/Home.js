b2314a2c50598d83e36c3aaa8ef01f36b0f68cbd
Samson343 <sam.ban@gmail.com>
2023-04-07 12:12:50 -0600

    created some awesome images with transparent backgrounds

--- portfoliodelasam/src/Components/Home.js
+++ portfoliodelasam/src/Components/Home.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function Home ({ setRouteTo }) {
     return (
         <div className={styles.homeRoot}>
+            <img className = {styles.flowerImage} src="flowerVine2.png" alt="white flowers"></img>
             <span className={styles.email}>sam.ban@gmail.com</span>
             <p className={styles.accent1}></p>
             <p className={styles.accent2}></p>
@@ -13,7 +14,14 @@ function Home ({ setRouteTo }) {
             <p className={styles.accent4}></p>
             <p className={styles.accent5}></p>
             <p className={styles.accent6}></p>
-            
+            <img className = {styles.flowerImage} src="flowerVine3.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage} src="flowerVine4.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage} src="flowerVine5.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage} src="flowerVine6.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage} src="flowerVine7.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage} src="flowerVine8.png" alt="white flowers"></img>
+            <p className={styles.textExpand}>hello, my name is</p>
+
         <Link to = '/loading' className={styles.penisLink}>
               <button className={styles.button} 
                 onClick={() => {
