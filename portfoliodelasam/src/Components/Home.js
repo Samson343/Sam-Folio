e443712fd632a1dc56d946f797d1220e91da2e5d
Samson343 <sam.ban@gmail.com>
2023-04-10 22:57:51 -0600

    Started in on the about section
    added some animations that trigger based on scroll position. Fixed some misc responsiveness issues, but still a lot of work to do to make sure things stay where they should on small screens

--- portfoliodelasam/src/Components/Home.js
+++ portfoliodelasam/src/Components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from './Home.module.css'
 import { Link } from "react-router-dom";
 import { FiGithub } from 'react-icons/fi'
@@ -7,16 +7,25 @@ import { SiCodewars } from 'react-icons/si'
 
 
 function Home({ setRouteTo }) {
+    const [scrollPosition, setScrollPosition] = useState(window.scrollY);
+
+    useEffect(() => {
+        function handleScroll() {
+          setScrollPosition(window.scrollY); // Update the scroll position state
+          console.log("useEffect running")
+        }
+        window.addEventListener('scroll', handleScroll);
+        // return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
 
     return (
         <div className={styles.homeRoot}>
             <div className={`${styles.emailWrapper}`}>
                 <img className={styles.flowerImage8} src="flowerVine5.png" alt="white flowers"></img>
-                
                     <p className={`${styles.emailLine} ${styles.slideInTop}`}></p>
                     <a href="mailto:sam.ban@gmail.com" className={`${styles.email} ${styles.slideInTop}`}>sam.ban@gmail.com</a>
-                
             </div>
+            
             <div className={styles.centerWrapper}>
               <span className= {styles.borderTop}></span>
               <p className={styles.textExpand}>hello, my name is,</p>
@@ -29,40 +38,29 @@ function Home({ setRouteTo }) {
                     }}
                 >See my Work</button>
             </Link>
-              <span className= {styles.borderBottom}></span>
+            <span className= {styles.borderBottom}></span>  
             </div>
-
-    
-
+            
             <div className={styles.topLeftWrapper}>
                 {/* <span className={styles.email}>sam.ban@gmail.com</span> */}
                 <FiGithub className={`${styles.socialIcons} ${styles.slideInBottom}`} size={'2.3vmin'} />
                 <SiCodewars className={`${styles.socialIcons} ${styles.slideInBottom}`} size={'2vmin'} />
                 <AiFillLinkedin className={`${styles.socialIcons} ${styles.slideInBottom}`} size={'2.3vmin'} />
 
-
                 <p className={`${styles.emailLine} ${styles.slideInBottom}`}></p>
                 <img className={styles.flowerImage7} src="flowerVine8.png" alt="white flowers"></img>
             </div>
-            <div className={styles.bottomLeftDiagonal}></div>
-            <div className={styles.topLeftDiagonal}></div>
+            <div className={`${styles.bottomLeftDiagonal}`}></div>
 
-            {/* <img className = {styles.flowerImage2} src="flowerVine2.png" alt="white flowers"></img> */}
-            <p className={styles.accent1}></p>
-            <p className={styles.accent2}></p>
-            <p className={styles.accent3}></p>
-            <p className={styles.accent4}></p>
-            <p className={styles.accent5}></p>
-            <p className={styles.accent6}></p>
-            {/* <img className = {styles.flowerImage3} src="flowerVine3.png" alt="white flowers"></img> */}
-            {/* <img className = {styles.flowerImage4} src="flowerVine4.png" alt="white flowers"></img> */}
-
-            {/* <img className = {styles.flowerImage5} src="flowerVine6.png" alt="white flowers"></img> */}
+            { scrollPosition >= 700 &&
+              <img className = {styles.flowerImage2} src="flowerVine2.png" alt="white flowers"></img>
+            }
+            {/* <img className = {styles.flowerImage3} src="flowerVine3.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage4} src="flowerVine4.png" alt="white flowers"></img>
+            <img className = {styles.flowerImage5} src="flowerVine6.png" alt="white flowers"></img> */}
             {/* <img className = {styles.flowerImage6} src="flowerVine7.png" alt="white flowers"></img> */}
-
-            
+            <div className={styles.aboutBackground}></div>
         </div>
-
     )
 }
 
