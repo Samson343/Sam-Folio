a95a534d70e8e474e123dad3cc97bd779f2efddd
Samson343 <sam.ban@gmail.com>
2023-04-16 17:47:23 -0600

    projects layout nearly complete
    also beefed up the about section, still needs a lot of work as far as responsiveness though

--- portfoliodelasam/src/Components/About.js
+++ portfoliodelasam/src/Components/About.js
@@ -1,55 +1,72 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from './About.module.css'
+import { FaGamepad, FaGuitar, FaHiking, FaRunning } from 'react-icons/fa'
+import { GiGuitarBassHead, GiRunningShoe } from 'react-icons/gi'
 
 
-function About ({ setRouteTo, scrollPosition }) {
+function About({ setRouteTo, scrollPosition }) {
     return (
-      <div className={styles.aboutRoot}>
+        <div className={styles.aboutRoot}>
 
-      <div className={styles.aboutHeaderWrapper}>
-          <h3 className={styles.aboutHeader}>
-              <span className={styles.numbers}>.01</span >
-              About me
-          </h3>
-          <span className={styles.aboutHeaderLine}></span>
-      </div>
-      <hr className={styles.aboutHr}></hr>
-      <div className={styles.aboutBackground}>
-          
-          <p className={styles.aboutDescription}>
-          <h3 className={styles.innerHeader}>I came here to build.</h3>
-              I'm a career changer jumping into tech from a background of dietetics. Since I'm late to the party I know I have to show up bearing gifts to make up for it. That's why I study late into the night, get a handle on the bleeding edge of tech, and pour my heart into everything I create. </p>
-          <div className={styles.listWrapper}>
-              <ul className={styles.techList}> Frontend
-                  <li className={styles.li}>React</li>
-                  <li className={styles.li}>CSS</li>
-                  <li className={styles.li}>HTML5</li>
-              </ul>
-              <ul className={styles.techList}> Backend
-                  <li className={styles.li}>SQL</li>
-                  <li className={styles.li}>Express</li>
-                  <li className={styles.li}>Node</li>
-              </ul>
-          </div>
-      </div>
-      <img className={styles.aboutPicture} src="Sam-about9.jpg" alt="headshot"></img>
-      <div className={styles.aboutPictureBackground}></div>
+            <div className={styles.aboutHeaderWrapper}>
+                <h3 className={styles.aboutHeader}>
+                    <span className={styles.numbers}>.01</span >
+                    About me
+                </h3>
+                <span className={styles.aboutHeaderLine}></span>
+            </div>
+            <hr className={styles.aboutHr}></hr>
+
+            <div className={styles.relative}>
+
+                <div className={styles.title}>I came here to build.</div>
+
+                <div className={styles.aboutBackground}>
+                    <div className={styles.interestsWrapper}>
+                        <div className={styles.interests}>
+                            <span>Things I enjoy: </span>
+                            <GiGuitarBassHead className={styles.interestIcons} />
+                            <GiRunningShoe className={styles.interestIcons} />
+                            <FaGamepad className={styles.interestIcons} />
+                            <FaHiking className={styles.interestIcons} />
+                        </div>
+
+                        <p className={styles.aboutDescription}>
+
+                            I'm a career changer jumping into tech from a background of dietetics. Since I'm late to the party I know I have to show up bearing gifts to make up for it. That's why I study late into the night, get a handle on the bleeding edge of tech, and pour my heart into everything I create. </p>
+                    </div>
+                    <div className={styles.listWrapper}>
+                        <ul className={styles.techList}> Frontend
+                            <li className={styles.li}>React</li>
+                            <li className={styles.li}>CSS</li>
+                            <li className={styles.li}>HTML5</li>
+                        </ul>
+                        <ul className={styles.techList}> Backend
+                            <li className={styles.li}>SQL</li>
+                            <li className={styles.li}>Express</li>
+                            <li className={styles.li}>Node</li>
+                        </ul>
+                    </div>
+                </div>
+                <img className={styles.aboutPicture} src="Sam-about9.jpg" alt="headshot"></img>
+                <div className={styles.aboutPictureBackground}></div>
+            </div>
 
 
-      {scrollPosition >= 700 &&
-          <img className={styles.flowerImage2} src="flowerVine2.png" alt="white flowers"></img>
-      }
-       {/* <Link to = '/loading'>
+            {scrollPosition >= 700 &&
+                <img className={styles.flowerImage2} src="flowerVine2.png" alt="white flowers"></img>
+            }
+            {/* <Link to = '/loading'>
               <button
                 onClick={() => {
                     setRouteTo('/home')
                 }}
               > Go home </button>
             </Link> */}
-  </div>
-           
-        
+        </div>
+
+
     )
 }
 
--- portfoliodelasam/src/Components/Projects.js
+++ portfoliodelasam/src/Components/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from './Projects.module.css'
+import { DiPostgresql, DiReact, DiNodejsSmall, DiNodejs, DiBootstrap, DiJavascript1, DiAndroid, DiCss3, DiHtml5, DiJavascript,   } from 'react-icons/di'
+import { SiExpress } from "react-icons/si";
 
 function Projects() {
     return (
@@ -24,6 +26,18 @@ function Projects() {
 
                             Features a dynamic cart and account system which allows users to pick up where they left off on different devices, experience a simulated checkout, and track their order history. An admin interface was created for inventory management- such as adding or removing products- in addition to warehouse stock tracking which is stored and persisted for every available product ensuring that orders cannot be created unless they can be fulfilled.</span>
 
+                            <div className={styles.techIcons}>
+                               <DiReact className={styles.techIcon}/>
+                               
+                               <SiExpress className={styles.techIcon}/>
+                               <DiNodejsSmall className={styles.techIcon}/>
+                               <DiPostgresql className={styles.techIcon}/>
+                               <DiBootstrap className={styles.techIcon}/>
+                               <DiCss3 className={styles.techIcon}/>
+                               <DiHtml5 className={styles.techIcon}/>
+                               
+                            </div>
+
                         <div className={styles.pictureBackground}></div>
 
                     </div>
@@ -34,6 +48,13 @@ function Projects() {
                         <div className={styles.titleRight}>Connected - Browser Game</div>
                         <span className={styles.projectDescription2}>An enjoyable browser game built from the ground up. Features a fully functional, home-grown, AI opponent that capitalizes on winning setups and blocks your attempts to win. The goal was to demo my Javascript skills through a simple game with a relatively complex logical framework as it relates to the DOM. Please feel free and hop on and play a game against that insidious AI!</span>
 
+                        <div className={styles.techIcons2}>
+                               <DiJavascript1 className={styles.techIcon}/>
+                               <DiCss3 className={styles.techIcon}/>
+                               <DiHtml5 className={styles.techIcon}/>
+
+                            </div>
+
                         <div className={styles.pictureBackground2}></div>
 
                     </div>
@@ -43,7 +64,21 @@ function Projects() {
                     <div className={styles.projectBackground}>
                         <div className={styles.title}>Stranger's Things Inc.</div>
 
-                        <span className={styles.projectDescription}></span>
+                        <span className={styles.projectDescription}> A web-app that aims to mirror the intricacies and subtle complexities of Craigslist, built from the ground up. Users can
+                            post items for sale, edit existing posts, message sellers to inquire about their items, and so much more!
+                            ● In-depth REST API integration that handles user authentication, creating and editing posts, and sending
+                            messages to other users on the platform.
+                            ● Designed and implemented a user-friendly UI with all CSS written from scratch </span>
+
+                            <div className={styles.techIcons}>
+                               <DiReact className={styles.techIcon}/>
+                               <SiExpress className={styles.techIcon}/>
+                               <DiNodejsSmall className={styles.techIcon}/>
+                               <DiPostgresql className={styles.techIcon}/>
+                               <DiCss3 className={styles.techIcon}/>
+                               <DiHtml5 className={styles.techIcon}/>
+
+                            </div>
 
                         <div className={styles.pictureBackground}></div>
 
