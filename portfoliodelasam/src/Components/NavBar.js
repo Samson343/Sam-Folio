70802d83f9d1929619f6384c055c4be6d8f003c5
Samson343 <sam.ban@gmail.com>
2023-04-19 12:52:24 -0600

    Fixed a mailTo link in the header

--- portfoliodelasam/src/Components/NavBar.js
+++ portfoliodelasam/src/Components/NavBar.js
@@ -35,7 +35,7 @@ function NavBar ({ rotateBox, setRotateBox }) {
             </div>
             <div className={styles.linkWrapper}>
                 <span className={styles.numbers}>.03</span>
-                <span className={styles.navLink}>Get in touch</span>
+                <a href="mailto:sam.ban@gmail.com" className={styles.navLink}>Get in touch</a>
             </div>
             <div className={styles.linkWrapper}>
                 <span className={styles.numbers}>.04</span>
