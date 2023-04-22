10bcc54417ceea2636a673618963500a4cb2e1ce
Samson343 <sam.ban@gmail.com>
2023-04-21 23:20:50 -0600

    installed scroll magic
    created an awesome scene for recommendations with independently scrolling elements

--- portfoliodelasam/src/Components/Contact.js
+++ portfoliodelasam/src/Components/Contact.js
@@ -1,32 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from './Contact.module.css'
-import { GiCampfire, GiFireAce, GiFireAxe, GiFireBomb, GiFireGem, GiFireWave, GiSmallFire } from 'react-icons/gi'
-import { TbCampfire } from 'react-icons/tb'
-// import { Link } from "react-router-dom";
-import {IoIosBonfire, IoMdBonfire} from 'react-icons/io'
-import {SiFireship} from 'react-icons/si'
-import {ImFire} from 'react-icons/im'
+import { Controller, Scene } from 'react-scrollmagic-r18';
+import { BsFillChatRightQuoteFill } from 'react-icons/bs'
 
 
 
-function Contact () {
+
+function Contact() {
+
+
+
     return (
         <div className={styles.contactRoot}>
+            <div className={styles.fireWrapper}>
+                <div className={styles.backgroundOpacifier}>
+                    <Controller>
+                        <Scene duration={990} triggerHook={0.3} pin>
+                            <div className={styles.centerWrapper}>
+                                <BsFillChatRightQuoteFill size={"8vmin"} className={styles.quoteIcon} />
 
-          <div className={styles.fireWrapper}>
-            <div className={styles.backgroundOpacifier}>
-            {/* <TbCampfire size={'10vh'} className={styles.campfireIcon}/>
-            <GiCampfire size={'10vh'} className={styles.campfireIcon}/>
-          
-           <GiFireGem size={'10vh'} className={styles.campfireIcon}/>
-           <GiFireWave size={'10vh'} className={styles.campfireIcon}/>
-           <GiSmallFire size={'10vh'} className={styles.campfireIcon}/> */}
-           {/* <IoMdBonfire size={'10vh'} className={styles.campfireIcon}/> */}
-           </div>
-          </div>
-          {/* <IoIosBonfire size={'10vh'} className={styles.campfireIcon}/> */}
-          
-          {/* <img className={styles.campfireIconLeft} src='fireGem.png' alt = 'fire gem'></img> */}
+                                <div className={styles.centerBox}>
+                                    <p>Sam was an exceptional student during his time at Fullstack Academy, consistently demonstrating a high level of intelligence and work ethic.</p>
+                                </div>
+
+                            </div>
+                        </Scene>
+                    </Controller>
+                    <div className={styles.pictureWrapper}>
+                        <img src="headshotAdam.png" className={styles.headshot} alt="Adam Marley"></img>
+                    </div>
+                    {/* <Controller>
+                        <Scene duration={150} triggerHook={0.5} pin>
+                            <div className={styles.pictureWrapper}>
+                                <img src="headshotAdam.png" className={styles.headshot} alt="Adam Marley"></img>
+                            </div>
+                        </Scene>
+                    </Controller> */}
+                </div>
+                <div className={styles.backgroundOpacifier}>
+                    <Controller>
+                        <Scene duration={850} triggerHook={0.3} pin>
+                            <div className={styles.centerWrapper}>
+                                <BsFillChatRightQuoteFill size={"8vmin"} className={styles.quoteIcon} />
+
+                                <div className={styles.centerBox}>
+                                    <p>I'm an inovator that would love to keep your company modern and relavent. If you like what you see, let's connect.</p>
+                                </div>
+                            </div>
+                        </Scene>
+                    </Controller>
+                </div>
+                <div className={styles.backgroundOpacifier}>
+                    <Controller>
+                        <Scene duration={850} triggerHook={0.3} pin>
+                            <div className={styles.centerWrapper}>
+                                <BsFillChatRightQuoteFill size={"8vmin"} className={styles.quoteIcon} />
+
+                                <div className={styles.centerBox}>
+                                    <p>I'm an inovator that would love to keep your company modern and relavent. If you like what you see, let's connect.</p>
+                                </div>
+                            </div>
+                        </Scene>
+                    </Controller>
+                </div>
+            </div>
         </div>
     )
 }
